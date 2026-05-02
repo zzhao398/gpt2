754ad61dd1be67cdb1f0a20073f9689bb9e17cfe
Zhenhan Zhao <zzhao21@dt-login04.delta.ncsa.illinois.edu>
2026-05-02 18:38:32 -0500

    Add op_7 and op_12 matmul optimizations

---
+++ kernels_op_7/matmul.cuh
@@ -0,0 +1,68 @@
+#ifndef __MATMUL_KERNEL_CUH__
+#define __MATMUL_KERNEL_CUH__
+
+#include <cuda_runtime.h>
+#include "../utils/cuda_utils.cuh"
+
+#define U 32
+#define BLOCK_SIZE 256
+#define TILE_WIDTH 8
+
+__global__ void matmul_forward_kernel(float* out, const float* inp, const float* weight,
+                                      const float* bias, int B, int T, int C, int OC) {
+    int bt = blockDim.x * blockIdx.x + threadIdx.x;
+    int oc_start = blockIdx.y * U;
+    int oc_offset = threadIdx.x % U;
+    int C_offset = threadIdx.x / U;
+    __shared__ float input_s[BLOCK_SIZE][TILE_WIDTH];
+    __shared__ float weight_s[TILE_WIDTH][U];
+    float sum[U];
+    for (int i = 0; i < U; i++) {
+        sum[i] = 0.0f;
+    }
+    for (int i = 0; i < (C - 1) / TILE_WIDTH + 1; i++) {
+        if (bt < B * T) {
+            for (int j = 0; j < TILE_WIDTH; j++) {
+                if (i * TILE_WIDTH + j < C)
+                    input_s[threadIdx.x][j] = inp[bt * C + i * TILE_WIDTH + j];
+                else
+                    input_s[threadIdx.x][j] = 0.0f;
+            }
+        } else {
+            for (int j = 0; j < TILE_WIDTH; j++) {
+                input_s[threadIdx.x][j] = 0.0f;
+            }
+        }
+        if (i * TILE_WIDTH + C_offset >= C || oc_offset + oc_start >= OC) {
+            weight_s[C_offset][oc_offset] = 0.0f;
+        } else {
+            weight_s[C_offset][oc_offset] = weight[(oc_offset + oc_start) * C + i * TILE_WIDTH + C_offset];
+        }
+        __syncthreads();
+        for (int j = 0; j < U; j++) {
+            for (int k = 0; k < TILE_WIDTH; k++) {
+                sum[j] += input_s[threadIdx.x][k] * weight_s[k][j];
+            }
+        }
+        __syncthreads();
+    }
+    for (int i = 0; i < U; i++) {
+        if (bias && oc_start + i < OC)
+            sum[i] += bias[oc_start + i];
+        if (bt < B * T && oc_start + i < OC)
+            out[bt * OC + oc_start + i] = sum[i];
+    }
+}
+
+void matmul_forward(float* out, const float* inp, const float* weight, const float* bias,
+                    int B, int T, int C, int OC) {
+    const unsigned int numThreadsPerBlock = BLOCK_SIZE;
+    dim3 numBlocks((B * T - 1) / numThreadsPerBlock + 1, (OC - 1) / U + 1);
+    matmul_forward_kernel<<<numBlocks, numThreadsPerBlock>>>(out, inp, weight, bias, B, T, C, OC);
+}
+
+#undef U
+#undef BLOCK_SIZE
+#undef TILE_WIDTH
+
+#endif // __MATMUL_KERNEL_CUH__
